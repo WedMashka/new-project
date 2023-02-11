5002f3187fd38b175adaf391ed5158c1390e2910
ionasmith <ionasmith@yandex.ru>
2023-02-11 23:27:09 +0300

    create hren poimi chto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 "use strict"
 
+function showAboutMe() {
+   let x = document.getElementById('aboutAdmin');
+   if (x.classList.contains('about__admin')) {
+      x.classList.remove('about__admin');
+      x.classList.toggle('hidden__box');
+   } else {
+      x.classList.remove('hidden__box');
+      x.classList.toggle('about__admin');
+   }
+}
 
